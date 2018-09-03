b87a62c7ea6fb4c4e02342a4649d66992ce2253a
mathildebuenerd <mathilde.buenerd@gmail.com>
2018-09-03 17:57:58 +0200

    finalae arduino animations and change the folder names in processing

--- emotionalRadio/emotionalRadio.pde
+++ emotionalRadio/emotionalRadio.pde
@@ -25,21 +25,19 @@ ArrayList emotions;
 
 // Emotion list, corresonding to the wekinator classifiers outputs
 String[] emotionsList = {
-  "calm", // classifier = 1
-  "happy", // classifier = 2
-  "sad" // classifier = 3
+  "idle", // classifier = 1
+  "smile", // classifier = 2
+  "surprised" // classifier = 3
 }; 
 
 int currentClassifier;
 int lastClassifier;
 
-String mirrorStatus = "on";
-
 ArrayList classfrValues;
 
 void setup()
 {
-  size(200, 200); 
+  size(400, 400); 
 
 
   emotions = new ArrayList();
@@ -47,10 +45,8 @@ void setup()
   lastClassifier = 1;
   currentClassifier = 1; // default emotion
   osc = new OscP5(this, 12000); // we listen to port 12000
-
-  String portName = Serial.list()[1]; // Set the port
-
-  //println(Serial.list());
+  
+  String portName = Serial.list()[0]; // Set the port
   myPort = new Serial(this, portName, 9600);
 
   // We add soundFiles to Emotion objects
@@ -60,66 +56,29 @@ void setup()
     Emotion newEmotion = new Emotion(emotionsList[i], files.length, soundFiles);
     emotions.add(newEmotion);
   }
-
-  // Starts with the default music
-  Emotion emotion = (Emotion)emotions.get(currentClassifier-1); // we get the current emotion, as the arraylist starts at 0 and the classifiers at 1, we remove 1
-  emotion.playRandomSound();
-  emotion.lightLED(str(currentClassifier));
-
-  drawStopButton();
-}
-
-void drawStopButton() {
-  // Draw the stop button
-  fill(255, 0, 0);
-  rect(15, 15, 70, 70);
-  textSize(27);
-  fill(255);
-  text("stop", 25, 50);
 }
 
 void draw() 
 {
-  if (mirrorStatus == "off") {
-    lightOff();
-  }
-}
-
-void mousePressed() {
-
-  // Switch the mirror status
-  if (mirrorStatus == "on") {
-    mirrorStatus = "off";
-  } else {
-    mirrorStatus = "on";
-  }
-}
-
-void lightOff() {
-  myPort.write("9");
 }
 
 
 void oscEvent(OscMessage msg) {
   // executed everytime Processing detects a message on port 12000
 
-  if (mirrorStatus == "on") {
-    String address = msg.addrPattern();
+  String address = msg.addrPattern();
 
-    if (address.equals("/wek/outputs")) { // look if the message comes from wekinator
-      currentClassifier = (int) msg.get(0).floatValue(); // a number like 1, 2, 3
-      println(currentClassifier);
-
-      if (currentClassifier != lastClassifier) { // if the classifier is different than the one before, we change the sound
-        Emotion emotion = (Emotion)emotions.get(currentClassifier-1); // we get the current emotion, as the arraylist starts at 0 and the classifiers at 1, we remove 1
-        clearSound(); // then we clear all sounds
-        emotion.playRandomSound();
-        emotion.lightLED(str(currentClassifier));
-      }
-      lastClassifier = currentClassifier;
+  if (address.equals("/wek/outputs")) { // look if the message comes from wekinator
+    currentClassifier = (int) msg.get(0).floatValue(); // a number like 1, 2, 3
+    println(currentClassifier);
+    
+    if (currentClassifier != lastClassifier) { // if the classifier is different than the one before, we change the sound
+      Emotion emotion = (Emotion)emotions.get(currentClassifier-1); // we get the current emotion, as the arraylist starts at 0 and the classifiers at 1, we remove 1
+      clearSound(); // then we clear all sounds
+      emotion.playRandomSound();
+      emotion.lightLED(str(currentClassifier));
     }
-  } else {
-    lightOff();
+    lastClassifier = currentClassifier;
   }
 }
 
--- mirror/mirror.ino
+++ mirror/mirror.ino
@@ -16,10 +16,7 @@ int colors [8] [3] = {
   {BRIGHTNESS, BRIGHTNESS, BRIGHTNESS} // White
 };
 
-int counter;
-
 char classifier; // Classifier received from Processing
-
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PINNeoPixel, NEO_GRBW + NEO_KHZ800);
 
@@ -35,11 +32,9 @@ void setup() {
   lastClassifier = 0;
   classifier = 1;
   isAnimationFinished = true;
-
-
 }
 
-void effaceTout() {
+void clearAll() {
   for (int i = 0; i < NUMPIXELS ; i++) {
     pixels.setPixelColor(i, pixels.Color(0, 0, 0));
   }
@@ -49,31 +44,28 @@ void effaceTout() {
 
 void loop() {
 
-
-
   if (Serial.available()) {
     classifier = Serial.read();
   }
 
-
   // If there is no animation at the time
   if (isAnimationFinished || classifier != lastClassifier) {
 
-    effaceTout();
+    clearAll();
     if (classifier == '1') {
-      sky(1);
-    } else if (classifier == '2') {
       sparkle(100, 100, 255);
+    } else if (classifier == '2') {
+      rainbow(1);
     } else if (classifier == '3') {
-      // Effect by https://www.tweaking4all.com/hardware/arduino/adruino-led-strip-effects/
-      Fire(55, 120, 15);
-    } else if (classifier = '9') {
-      effaceTout();
+      sky(2);
+    } else if (classifier = '9') { 
+      // For lighting off the mirror
+      clearAll();
     }
     lastClassifier = classifier;
 
   } else {
-    effaceTout();
+    clearAll();
   }
 
 }
@@ -84,13 +76,10 @@ void sparkle(int red, int green, int blue) {
   int i, j;
   isAnimationFinished = false;
 
-
   for (j = 0; j < 10; j++) {
-    effaceTout();
+    clearAll();
     for (i = 0; i < 5; i++) {
       int id = int(random(NUMPIXELS));
-      //byte col[3];
-      //chooseColor(col);
       pixels.setPixelColor(id, random(red), random(green), random(blue));
     }
     pixels.show();
@@ -102,66 +91,13 @@ void sparkle(int red, int green, int blue) {
 }
 
 
-void chooseColor(byte pcol[]) {
-  int r = int(random(BRIGHTNESS));
-  int g = int(random(BRIGHTNESS));
-  int b = int(random(BRIGHTNESS));
-  pcol[0] = r;
-  pcol[1] = g;
-  pcol[2] = b;
-}
-
-int chooseRandomColor() {
-  int dice = int(random(10));
-  if (dice == 0) {
-    return 0;
-  } else if (dice == 1) {
-    return 1;
-  } else if (dice == 2) {
-    return 3;
-  } else if (dice == 3) {
-    return 4;
-  } else {
-    return 2;
-  }
-
-}
-
-void singleColor(String col) {
-  effaceTout();
-  for (int i = 0; i < NUMPIXELS; i++) {
-    int ledId = i;
-    if (col == "blue") {
-      pixels.setPixelColor(ledId, pixels.Color(0, 0, BRIGHTNESS));
-    } else if (col == "red") {
-      pixels.setPixelColor(ledId, pixels.Color(20, 0, 0));
-    } else if (col == "green") {
-      pixels.setPixelColor(ledId, pixels.Color(0, 20, 0));
-    }
-  }
-}
-
-void hasard() {
-  effaceTout();
-  for (int i = 0; i < 50; i++) {
-    int ledId = int(random(NUMPIXELS));
-    color = chooseRandomColor();
-    pixels.setPixelColor(ledId, pixels.Color(colors[color][0], colors[color][1], colors[color][2]));
-  }
-  pixels.show();
-}
-
 void sky(uint8_t wait) {
   uint16_t i, j;
   isAnimationFinished = false;
 
   for (j = 0; j < 256; j++) {
     for (i = 0; i < pixels.numPixels(); i++) {
-      pixels.setPixelColor(i, Wheel((i + j) & -100));
-      //int newj = map(j, 0, 256, 128, 170);
-      //Serial.println(j);
-      //Serial.println(newj);
-      // pixels.setPixelColor(i, j+i, j+i,255);
+      pixels.setPixelColor(i, SkyWheel((i + j) & -100));
     }
     pixels.show();
     delay(wait);
@@ -174,12 +110,12 @@ void sparklingSky(uint8_t wait) {
   uint16_t i, j;
   isAnimationFinished = false;
 
-  effaceTout();
+  clearAll();
 
   for (j = 0; j < 256; j++) {
     for (i = 0; i < 10; i++) {
       int randomPixel = int(random(pixels.numPixels()));
-      pixels.setPixelColor(randomPixel, Wheel((i + j) & -100));
+      pixels.setPixelColor(randomPixel, SkyWheel((i + j) & -100));
       //int newj = map(j, 0, 256, 128, 170);
       //Serial.println(j);
       //Serial.println(newj);
@@ -198,7 +134,7 @@ void rainbow(uint8_t wait) {
 
   for (j = 0; j < 256; j++) {
     for (i = 0; i < pixels.numPixels(); i++) {
-      pixels.setPixelColor(i, Wheel((i + j) & 255));
+      pixels.setPixelColor(i, RainbowWheel((i + j) & 255));
     }
     pixels.show();
     delay(wait);
@@ -210,73 +146,20 @@ void rainbow(uint8_t wait) {
 
 // Input a value 0 to 255 to get a color value.
 // The colours are a transition r - g - b - back to r.
-uint32_t Wheel(byte WheelPos) {
+uint32_t SkyWheel(byte WheelPos) {
   WheelPos = 255 - WheelPos;
-  //if (WheelPos < 85) {
   return pixels.Color(255 - WheelPos * 3, 240 - WheelPos * 3, 255);
-  //}
-  //if (WheelPos < 170) {
-  //WheelPos -= 85;
-  //return pixels.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-  //}
-  //WheelPos -= 170;
-  //return pixels.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
 }
 
-void Fire(int Cooling, int Sparking, int SpeedDelay) {
-
-  static byte heat[NUMPIXELS];
-  int cooldown;
-  isAnimationFinished = false;
-
-
-  // Step 1.  Cool down every cell a little
-  for ( int i = 0; i < NUMPIXELS; i++) {
-    cooldown = random(0, ((Cooling * 10) / NUMPIXELS) + 2);
-
-    if (cooldown > heat[i]) {
-      heat[i] = 0;
-    } else {
-      heat[i] = heat[i] - cooldown;
-    }
+uint32_t RainbowWheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if (WheelPos < 85) {
+    return pixels.Color(255 - WheelPos * 3, 0, WheelPos * 3);
   }
-
-  // Step 2.  Heat from each cell drifts 'up' and diffuses a little
-  for ( int k = NUMPIXELS - 1; k >= 2; k--) {
-    heat[k] = (heat[k - 1] + heat[k - 2] + heat[k - 2]) / 3;
+  if (WheelPos < 170) {
+    WheelPos -= 85;
+    return pixels.Color(0, WheelPos * 3, 255 - WheelPos * 3);
   }
-
-  // Step 3.  Randomly ignite new 'sparks' near the bottom
-  if ( random(255) < Sparking ) {
-    int y = random(7);
-    heat[y] = heat[y] + random(160, 255);
-    //heat[y] = random(160,255);
-  }
-
-  // Step 4.  Convert heat to LED colors
-  for ( int j = 0; j < NUMPIXELS; j++) {
-    setPixelHeatColor(j, heat[j] );
-  }
-  pixels.show();
-  delay(SpeedDelay);
-  isAnimationFinished = true;
-}
-
-void setPixelHeatColor (int Pixel, byte temperature) {
-
-  // Scale 'heat' down from 0-255 to 0-191
-  byte t192 = round((temperature / 255.0) * 191);
-
-  // calculate ramp up from
-  byte heatramp = t192 & 0x3F; // 0..63
-  heatramp <<= 2; // scale up to 0..252
-
-  // figure out which third of the spectrum we're in:
-  if ( t192 > 0x80) {                    // hottest
-    pixels.setPixelColor(Pixel, 255, 255, heatramp);
-  } else if ( t192 > 0x40 ) {            // middle
-    pixels.setPixelColor(Pixel, 255, heatramp, 0);
-  } else {                               // coolest
-    pixels.setPixelColor(Pixel, heatramp, 0, 0);
-  }
+  WheelPos -= 170;
+  return pixels.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
 }
